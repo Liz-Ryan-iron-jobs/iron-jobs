ecdacee7da0ffea82ddead382f8d05647c06a5a1
Ryan Cronin <Ryan@Ryans-MBP.lan>
2017-04-12 17:33:29 -0400

    added route, express, listen(nodemon)  and parser

--- server/app.js
+++ server/app.js
@@ -1,2 +1,12 @@
+const  express = require('express');
+const bodyParser = require('body-parser');
 
-console.log('Hello World');
+let app = express();
+
+app.use(bodyParser.json());
+app.use(require('./middleware/auth.middleware.js'));
+
+app.use('/api/job', require('./routes/job.routes.js'));
+app.listen(3000, function doSomethingOnceServerIsUp(){
+  console.log('The server is now up!');
+});
