a3fa26c40555db4d0ee42595945d641cfd040dd9
Ryan Cronin <Ryan@Ryans-MBP.home>
2017-04-13 20:36:00 -0400

    updated with mongo

--- server/app.js
+++ server/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+require('./database-setup.js');
 let app = express();
+// console.log('process id', process.pid);
+// console.log('Enviroment Variables', process.env);
+console.log('Database Location', process.env.MONGO_DB);
 
+app.use(express.static(__dirname + '/../client/public'));
 app.use(bodyParser.json());
 app.use(require('./middleware/auth.middleware.js'));
 
-app.use('/api/job', require('./routes/job.routes.js'));
+app.use('/api/jobs', require('./routes/job.routes.js'));
+
+
+app.use(require('./middleware/error-handler.middleware.js'));
 app.listen(3000, function doSomethingOnceServerIsUp(){
   console.log('The server is now up!');
 });
